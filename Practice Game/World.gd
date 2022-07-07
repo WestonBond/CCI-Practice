837997a6edd01cd9954cf384dac01c7ca4bd1c7a
Philip Smith <philip@randomsmiths.com>
2022-07-07 09:52:35 -0400

    started working on obstacles

--- Practice Game/World.gd
+++ Practice Game/World.gd
@@ -1,7 +1,32 @@
 extends Node2D
+export(PackedScene) var rock_scene
 
+var score
 
 func _ready():
-	pass 
+	randomize()
+	new_game() 
+	
+
+func new_game():
+	score = 0
+	$StartTimer.start()
+	
 
 
+
+
+func _on_StartTimer_timeout():
+	$RockTimer.start()
+	$ScoreTimer.start()
+
+
+func _on_ScoreTimer_timeout():
+	score += 1 
+
+
+func _on_RockTimer_timeout():
+	var rock = rock_scene.instance()
+	rock.position.x = 600
+	rock.position.y = (randi() % 3) * 10
+	add_child(rock)
