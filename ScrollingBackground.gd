be58d3231ecff0b85ff35def43d8cabb795dfcff
kaywayz <owenmctoen@gmail.com>
2022-07-06 23:31:11 -0400

    Hopefully fixing things

--- ScrollingBackground.gd
+++
@@ -1,8 +0,0 @@
-extends ParallaxBackground
-
-var scrolling_speed = -200
-
-#Makes screen scroll based on predefined scrolling speed
-func _process(delta):
-	scroll_offset.x += scrolling_speed * delta
-
--- Train.gd
+++
@@ -1,6 +0,0 @@
-extends KinematicBody2D
-
-func _ready():
-	pass
-
-# TODO: Add functionality for moving up and down to other tracks
